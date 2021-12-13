311fc01f5cdcc83ef935da1406fbde8b2c9f2515
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-12-14 02:40:47 +0900

    Add st.download_button

--- file-upload/streamlit-app.py
+++ file-upload/streamlit-app.py
@@ -5,3 +5,5 @@ st.title("App Engine sample app")
 uploaded_file = st.file_uploader("Upload some file")
 if uploaded_file:
     st.write(f"{uploaded_file.name} was uploaded.")
+
+    st.download_button(f"Download {uploaded_file.name}", data=uploaded_file, file_name=uploaded_file.name)
